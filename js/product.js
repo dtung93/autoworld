eb4a3e587babe48f075ce94ef54bd5b5638a09e1
dtung93 <89057251+dtung93@users.noreply.github.com>
2021-08-17 11:00:06 +0700

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,35 @@
+
+   $(document).ready(
+ function(){
+     var x='https://60d715b1307c300017a5f624.mockapi.io'
+ $.ajax({
+ url: x+'/'+'data',
+ type:'get',
+ dataType:'json',
+success:function(s){
+ console.log(s)
+ $.each(s,function(x1,x2){
+console.log(s)
+ b='<div class=col-sm-6 col-md-3>'+x2.name+'<img src="'+x2.avatar+'"</div>'+'<h5 style="display:inline">'+x2.price+'</h5>'+'<br>'+'<button type="button"  data-toggle="modal" data-target="#detail"  class="btn btn-info" onclick="F1('+x2.id+')">Details</button>'
+ b+='&nbsp'+'<button class="btn btn-primary" type="button" onclick="Compare('+x2.id+')">Compare</button>'
+ $('.products').append(b)
+
+
+ })
+
+
+
+ }
+
+
+
+
+ })
+
+
+ }
+
+
+     )
+
+
