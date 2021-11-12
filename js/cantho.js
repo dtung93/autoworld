f8234222d21be6e8a2b738342a8e2a29d2126d8c
dtung93 <89057251+dtung93@users.noreply.github.com>
2021-11-12 12:51:54 +0700

    Add files via upload

--- js/cantho.js
+++ js/cantho.js
@@ -10,10 +10,8 @@
     console.log(s)
    
     $.each(s,function(x1,x2){
-        b='<div class="col-sm-6 col-md-3" id="pro_'+x2.id+'" style="font-weight:bold">'+ '<span class="name">'+x2.name+"</span>"+'<img src="'+x2.avatar+'">'+'<span class="price">'+x2.price+'</span>'+'<br>'
-        b+= '<span class="fa fa-star checked">'+'</span>'+'<span class="fa fa-star checked">'+'</span>'+'<span class="fa fa-star checked">'+'</span>'
-        b+= '<span class="fa fa-star checked">'+'</span>'+'<span class="fa fa-star checked">'+'</span>'+'<br>'
-        b+='<button type="button"  data-toggle="modal" data-target="#detail"  class="btn btn-sm btn-info" onclick="F1('+x2.id+')">Details</button>'+'&nbsp'+'<button class="btn btn-danger btn-sm" type="button" onclick="addToCart('+x2.id+')" >Add To Cart</button>'+'</div>'
+        b='<div class="col-sm-12 col-md-3" style="font-weight:bold;cursor:pointer" id="pro_'+x2.id+'">'+'<span class="name" onclick="View('+x2.id+')">'+x2.name+'</span>'+'<img src="'+x2.avatar+'" onclick="View('+x2.id+')">'+'<span class="price" style="font-size:20px">'+x2.price+'</span>'+'<br>'
+        b+='<i class="fa fa-shopping-cart" id="productcart" aria-hidden="true" onclick="addToCart('+x2.id+')"></i>'
     if(x2.province=="Can Tho"){
     $('.products').append(b)}
    
@@ -35,5 +33,10 @@
    
         )
    
-   
+        function View(id)
+        {
+    console.log(id)
+    localStorage.setItem('product_id',id)
+    window.location='viewmore.html'
+        }
    
